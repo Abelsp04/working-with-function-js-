2569e88cb71f361ecff4db18631cc53a457aa71f
Abel Segui <Abelsegui@hotmail.com>
2020-06-18 03:37:07 +0000

    07done

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,4 +1,8 @@
-
+const rapid=(str)=>{
+    for(let i=0; i<=str.lenght;i++)
+    var str2=str.toLowerCase(str[i])
+//   return str.replace(/[aeiou]/i
+}
    // From this line up Do not change code below
    let str = "John";
    console.log(rapid(str));
